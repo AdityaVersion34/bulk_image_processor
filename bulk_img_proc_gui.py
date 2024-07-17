8ae4f95b3e2c718212dbccf6af0be39ac6280cc7
AdityaVersion34 <112605343+AdityaVersion34@users.noreply.github.com>
2024-07-17 17:19:51 +0530

    Update bulk_img_proc_gui.py

--- bulk_img_proc_gui.py
+++ bulk_img_proc_gui.py
@@ -1,6 +1,13 @@
 import tkinter as tk
+from tkinter.filedialog import askopenfilename, askdirectory
 
 #This file contains python code for the main GUI of the bulk image processor
+
+def get_inp_dir():
+    filepath = askdirectory()
+    if not filepath:
+        return
+    lbl_confirm_inp_dir["text"] = filepath
 
 #initializing base gui window
 base_win = tk.Tk()
@@ -29,5 +36,18 @@ frm_get_dir.grid(row=0, column=0, padx=5, pady=5, sticky="news")
 frm_get_splits.grid(row=1, column=0, padx=5, pady=5, sticky="news")
 frm_run_model.grid(row=2, column=0, padx=5, pady=5, sticky="news")
 frm_result_dir.grid(row=3, column=0, padx=5, pady=5, sticky="news")
+
+#heading label for directory entry section
+lbl_get_dir_info = tk.Label(master=frm_get_dir, bg="white", relief=tk.SUNKEN, text="1) Enter the image input and output directories")
+
+lbl_get_inp_dir = tk.Label(master=frm_get_dir, bg="white", border=3, text="Enter the image input directory: ")
+btn_get_inp_dir = tk.Button(master=frm_get_dir, text="Select Directory...", command=get_inp_dir)
+lbl_confirm_inp_dir = tk.Label(master=frm_get_dir, bg="white", border=3, text="")
+
+lbl_get_dir_info.grid(row=0, column=0, ipadx=5, ipady=5, padx=2, pady=2, sticky="nw")
+
+lbl_get_inp_dir.grid(row=1, column=0, padx=2, pady=2, sticky="nw")
+btn_get_inp_dir.grid(row=1, column=1, padx=2, pady=2, sticky="nw")
+lbl_confirm_inp_dir.grid(row=1, column=2, padx=2, pady=2, sticky="nw")
 
 base_win.mainloop()
