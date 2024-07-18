e647c1e858f56fc3d5322c0b1f7092699c12c830
AdityaVersion34 <112605343+AdityaVersion34@users.noreply.github.com>
2024-07-18 12:22:26 +0530

    Update bulk_img_proc_gui.py

--- bulk_img_proc_gui.py
+++ bulk_img_proc_gui.py
@@ -3,11 +3,56 @@ from tkinter.filedialog import askopenfilename, askdirectory
 
 # This file contains python code for the main GUI of the bulk image processor
 
-def get_dir(where_to):
+def get_dir(*destinations) -> None:
+    '''
+    Asks to select a directory and print dir path to destinations
+    :param destinations: list of tkinter elements to which to print destinations
+    :return: None
+    '''
+
     filepath = askdirectory()
     if not filepath:
         return
-    where_to["text"] = filepath
+    for elem in destinations:
+        elem["text"] = filepath
+
+def confirm_pos_int(inp) -> bool:
+    '''
+    confirms that the parameter is a positive integer
+    :param inp:
+    :return:
+    '''
+
+    try:
+        inp_to_int = int(inp)
+        if (inp_to_int > 0):
+            return True
+        else:
+            return False
+    except:
+        return False
+
+def confirm_button(source, confirmer, err_field, *destinations):
+    '''
+    This function is meant to be used with a confirm button widget
+
+    It obtains data from source, checks it with a confirmer function. If true, copies to destinations.
+    If false, prints err msg to err field
+
+    :param source:
+    :param confirmer:
+    :param destinations:
+    :return:
+    '''
+
+    if not confirmer(source.get()):
+        err_field["text"] = "Please enter a positive integer"
+    else:
+        err_field["text"] = ""
+        for elem in destinations:
+            elem["text"] = source.get()
+
+
 
 # initializing base gui window
 base_win = tk.Tk()
@@ -45,7 +90,8 @@ lbl_get_dir_info = tk.Label(master=frm_get_dir, bg="white", relief=tk.SUNKEN, te
 
 # getting input directory
 lbl_get_inp_dir = tk.Label(master=frm_get_dir, bg="white", border=3, text="Enter the image input directory: ")
-btn_get_inp_dir = tk.Button(master=frm_get_dir, text="Select Directory...", command=lambda: get_dir(lbl_confirm_inp_dir))
+btn_get_inp_dir = tk.Button(master=frm_get_dir, text="Select Directory...",
+                            command=lambda: get_dir(lbl_confirm_inp_dir, lbl_inp_dir_overview_data))
 lbl_confirm_inp_dir = tk.Label(master=frm_get_dir, bg="white", border=3, text="")
 
 # getting output directory
@@ -72,22 +118,35 @@ lbl_get_preproc_info = tk.Label(master=frm_get_splits, bg="white", relief=tk.SUN
 lbl_get_splits = tk.Label(master=frm_get_splits, bg="white", border=3, text="Enter the number of image splits per dimension: ")
 ent_get_splits = tk.Entry(master=frm_get_splits, bg="white", width=7)
 lbl_split_err_msg = tk.Label(master=frm_get_splits, bg="white", border=3, text="")
+btn_split_confirmer = tk.Button(master=frm_get_splits, text="Confirm",
+                                command= lambda: confirm_button(ent_get_splits, confirm_pos_int, lbl_split_err_msg, lbl_split_no_overview_data))
 
 # adding to grid
 lbl_get_preproc_info.grid(row=0, column=0, ipadx=5, ipady=5, padx=2, pady=2, sticky="nw")
 
 lbl_get_splits.grid(row=1, column=0, padx=2, pady=2, sticky="nw")
 ent_get_splits.grid(row=1, column=1, padx=2, pady=2, sticky="nw")
+btn_split_confirmer.grid(row=1, column=2, padx=2, pady=2, sticky="nw")
+lbl_split_err_msg.grid(row=1, column=3, padx=2, pady=2, sticky="nw")
 
 # ===========================================================
 # EXECUTION INFO
 
+# heading
 lbl_run_model_info = tk.Label(master=frm_run_model, bg="white", relief=tk.SUNKEN, text="3) Review and process images")
 
+# overview labels
 lbl_run_model_warning = tk.Label(master=frm_run_model, bg="white", text="Please review your selections before proceeding: ")
 lbl_model_overview = tk.Label(master=frm_run_model, bg="white", text="\tModel Overview:\t")#VGG16, pretrained on Khanhha's Dataset")
 lbl_inp_dir_overview = tk.Label(master=frm_run_model, bg="white", text="\tInput directory:\t")
 lbl_split_no_overview = tk.Label(master=frm_run_model, bg="white", text="\tNumber of splits per dimension:\t")
+
+# overview data
+lbl_model_overview_data = tk.Label(master=frm_run_model, bg="white", text="VGG16, pretrained on Khanhha's Dataset")
+lbl_inp_dir_overview_data = tk.Label(master=frm_run_model, bg="white", text="")
+lbl_split_no_overview_data = tk.Label(master=frm_run_model, bg="white", text="")
+
+btn_proc_run = tk.Button(master=frm_run_model, bg="limegreen", text="Run")
 
 # adding to grid
 lbl_run_model_info.grid(row=0, column=0, ipadx=5, ipady=5, padx=2, pady=2, sticky="nw")
@@ -96,6 +155,12 @@ lbl_run_model_warning.grid(row=1, column=0, padx=2, pady=2, sticky="nw")
 lbl_model_overview.grid(row=2, column=0, padx=2, pady=2, sticky="nw")
 lbl_inp_dir_overview.grid(row=3, column=0, padx=2, pady=2, sticky="nw")
 lbl_split_no_overview.grid(row=4, column=0, padx=2, pady=2, sticky="nw")
+
+lbl_model_overview_data.grid(row=2, column=1, padx=2, pady=2, sticky="nw")
+lbl_inp_dir_overview_data.grid(row=3, column=1, padx=2, pady=2, sticky="nw")
+lbl_split_no_overview_data.grid(row=4, column=1, padx=2, pady=2, sticky="nw")
+
+btn_proc_run.grid(row=5, column=2, ipadx=10, ipady=4, padx=2, pady=2, sticky="nw")
 
 # ===========================================================
 
