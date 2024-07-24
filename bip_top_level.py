833f4913379866752c3ff2ce567d172de9256f25
AdityaVersion34 <112605343+AdityaVersion34@users.noreply.github.com>
2024-07-24 17:15:25 +0530

    Delete bip_top_level.py

--- bip_top_level.py
+++
@@ -1,5 +0,0 @@
-
-
-# This file will be at the top level of the bulk image processor.
-# It will handle the launching of a terminal console, and from there launch bulk_img_proc_gui.py. This should serve
-#   as a rudimentary way to obtain the tqdm progress bar. Ideally it would be integrated into
